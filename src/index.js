615e881588226cfb2c96b11d4286bfc966933d0b
Ruslan G <r.gusseinov@mail.ru>
2020-02-08 15:48:23 +0600

    added react code to guess-melody project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app/app.jsx';
+
+const Settings = {
+    ERROR_COUNT: 3
+};
+
+ReactDOM.render(
+    <App
+        errorsCount = {Setting.ERROR_COUNT}
+    />, 
+    document.querySelector(`#root`)
+)
