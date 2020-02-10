e5a0458ab1d3fbaf33b3af3b2ac65ef575a7ddbc
Ruslan G <r.gusseinov@mail.ru>
2020-02-11 00:01:38 +0600

    my frist XO game on react

---
+++ src/board.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Cell from "./cell.jsx";
+
+export default class Board extends React.Component {
+
+  renderCell(i) {
+    return <Cell value={i} onClick={() => console.log(`Clicked on cell`)} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderCell(0)}
+          {this.renderCell(1)}
+          {this.renderCell(2)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(3)}
+          {this.renderCell(4)}
+          {this.renderCell(5)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(6)}
+          {this.renderCell(7)}
+          {this.renderCell(8)}
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './components/app/app.jsx';
-
-const Settings = {
-  ERROR_COUNT: 3
-};
-
+import Game from "./game.jsx";
 
 ReactDOM.render(
-    <App
-      errorsCount = {Settings.ERROR_COUNT}
-    />,
-    document.querySelector(`#root`)
+    <Game />,
+    document.getElementById(`root`)
 );
