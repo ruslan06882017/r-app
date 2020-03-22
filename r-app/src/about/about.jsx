4ccd3d2c2ec7e47ba8985ed72aeecacd5291bb9e
Ruslan Gusseinov <r.gusseinov@mail.ru>
2020-03-23 01:01:29 +0600

    Added NavLinks

---
+++ r-app/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class About extends Component {
+    render(){
+        return (
+            <div className="content"> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Molestiae corporis earum id magni accusantium! 
+                 Earum sed ullam id iure numquam inventore delectus
+                  ipsa laudantium architecto voluptatibus, magni eius,
+                   placeat nostrum.
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ r-app/src/cars/cars.jsx
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+
+class Cars extends Component {
+    render(){
+        return (
+            <React.Fragment>
+                <div className="content">
+                    <div className="car">
+                        <h1>Audi</h1>
+                        <p>
+                         Achieving equal footing with the likes of luxury car titans BMW and Mercedes-Benz is no easy task, but it’s a challenge that Audi has both accepted and accomplished over the years. Audi first began standing out with the introduction of the quattro AWD system in the early 80s, which played a part in AWD becoming almost ubiquitous among luxury vehicles.
+                        </p>
+                    </div>
+
+                    <div className="car">
+                        <h1>Mazda</h1>
+                        <p>
+                          both accepted and accomplished over the years. an standing out with the introduction of the quattro AWD system in the early 80s, which played a part in AWD becoming almost ubiquitous among luxury vehicles.
+                        </p>
+                    </div>
+
+                    <div className="car">
+                        <h1>Nexia</h1>
+                        <p>
+                          over the years. Audi first began standing out with the introduction of the quattro AWD system in the early 80s, which played a part in AWD becoming almost ubiquitous among luxury vehicles.
+                        </p>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Cars;
