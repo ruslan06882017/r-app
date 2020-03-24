59b1233b48ed8565ba2e0db08b8b905e1d529354
Ruslan G <r.gusseinov@mail.ru>
2020-03-24 10:58:55 +0600

    Added Auth component

--- r-app/src/App.js
+++ r-app/src/App.js
@@ -1,30 +1,44 @@
 import React, {Component} from 'react';
 import Cars from './cars/cars.jsx';
 import CarDetail from './car-detail/car-detail.jsx';
+import Auth from './auth/auth.jsx';
 import About from './about/about.jsx';
 import { NavLink,Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <React.Fragment>
-      <div>
-        <nav>
-          <ul>
-            <li><NavLink to="/"> Home </NavLink></li>
-            <li><NavLink to="/cars"> Cars </NavLink></li>
-            <li><NavLink to="/about"> About </NavLink></li>
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route path="/" exact render={() => <h1>Home page</h1>} />
-        <Route path="/about" component={About} />
-        <Route path="/cars/:name" component={CarDetail} />
-        <Route path="/cars" component={Cars} />
-        <Redirect to={"/"} />
-      </Switch>
-    </React.Fragment>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  };
+  
+  render(){
+    return (
+      <React.Fragment>
+        <div>
+          <nav>
+            <ul>
+              <li><NavLink to="/"> Home </NavLink></li>
+              <li><NavLink to="/cars"> Cars </NavLink></li>
+              <li><NavLink to="/about"> About </NavLink></li>
+              <li><NavLink to="/auth"> Auth </NavLink></li>
+            </ul>
+          </nav>
+          <p>{this.state.loggedIn ? 'Hello, Ruslan' : 'Please log in'}</p>
+          <button onClick={() => this.setState({loggedIn: true})}> Log in </button>
+        </div>
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home page</h1>} />
+          {this.state.loggedIn ? <Route path="/about" component={About}/> : null}          
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+          <Route path="/auth" component={Auth} />
+          <Redirect to={"/"} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ r-app/src/auth/auth.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class Auth extends Component {
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+    render(){
+        return (
+            <div>
+                <h1>Auth</h1>
+                <form onSubmit={this.submitHandler}>
+                    <input type="text" placeholder="Log in"/> <br/>
+                    <input type="text" placeholder="Password"/> <br/>
+                    <button onClick={this.loginHandler}>Log in</button> 
+                    <button onClick={this.registerHandler}>Registration</button> 
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default Auth;
